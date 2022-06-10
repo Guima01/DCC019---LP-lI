53fcc73e3071a9b0e6e4521d4870cacd278baa5c
Guilherme Marques <guilhermemarques1@ice.ufjf.br>
2022-06-10 13:40:30 -0300

    Será que Leozinho foi baleado pelo segunda vez

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,9 @@ import System.Random (randomRIO)
 sumValue :: Int -> Int -> Int -> Int
 sumValue r t n = if r == t then n + 1 else n
 
+removeFirst :: [Int] -> [Int]
+removeFirst (x:xs) = xs
+
 compareCode :: [Int] -> [Int] -> Int
 compareCode [] _ = 0
 compareCode a b =
@@ -13,12 +16,63 @@ compareCode a b =
     else
         compareCode (tail a) (tail b)
 
+compareCode2 :: [Int] -> [Int] -> [Bool]
+compareCode2 [] _ = []
+compareCode2 a b = 
+    if head a == head b then
+        (True:compareCode2 (tail a) (tail b))
+    else
+        (False:compareCode2 (tail a) (tail b))
+
+compareElementPartial :: Int -> [Int] -> [Bool] -> [Bool]
+compareElementPartial _ [] _ = []
+compareElementPartial a b listBool =
+    if a /= head b || head listBool == True then
+        (False:compareElementPartial a (tail b) (tail listBool))
+    else
+        (True: listOfFalse (tail listBool))
+
+-- comparePartialCode :: [Int] -> [Int] -> [Bool] -> [Bool]
+-- comparePartialCode _ [] _ = []
+-- comparePartialCode a b listBool = do
+--     let bool2 = compareElementPartial (head a) b listBool
+    -- let a2 = removeFirst a
+    -- let bool2Adjusted = compareListBool (listBool) (bool2)
+    -- show bool2Adjusted
+    -- return (True)
+    -- bool3 <- compareElementPartial (head a2) (b) (bool2Adjusted)
+    -- return bool3
+
+
+listOfFalse :: [Bool] -> [Bool]
+listOfFalse []  = []
+listOfFalse a =
+    (False :listOfFalse (tail a))
+
+compareListBool :: [Bool] -> [Bool] -> [Bool]
+compareListBool [] [] = []
+compareListBool a b =
+    if head a /= head b then
+        (True: compareListBool (tail a) (tail b))
+    else
+        (False: compareListBool (tail a) (tail b))
+
 randomList :: Int -> IO([Int])
 randomList 0 = return []
 randomList n = do
   r  <- randomRIO (1,6)
   rs <- randomList (n-1)
   return (r:rs) 
+
+countResult :: [Bool] -> Int
+countResult [] = 0
+countResult a =
+    if head a == True then
+        1 + countResult (tail a)
+    else
+        countResult (tail a) 
+
+
 
 
 
@@ -27,7 +81,35 @@ main = do
     putStrLn "Please enter the code"
     line <- getLine  
     let userList = map (read::String->Int) (splitOn  " " line)
-    generatedList <- randomList 4
-    let completo = compareCode userList generatedList
-    print(show completo ++ " Completo, " ++ show 0 ++ " Parcial")
+    -- generatedList <- randomList 4
+    let generatedList = [6,6,2,1]
+    let resultCompleto = compareCode2 userList generatedList
+    let completo = countResult resultCompleto
+    -- 
+    let bool2 = compareElementPartial (head userList) (generatedList) (resultCompleto)
+    let a2 = removeFirst userList
+    let bool2Adjusted = compareListBool (resultCompleto) (bool2)
+    let bool3 = compareElementPartial (head a2) (generatedList) (bool2Adjusted)
+    let a3 = removeFirst a2
+    let bool3Adjusted = compareListBool (bool2Adjusted) (bool3)
+    let bool4 = compareElementPartial (head a3) (generatedList) (bool3Adjusted)
+    let a4 = removeFirst a3
+    let bool4Adjusted = compareListBool (bool3Adjusted) (bool4)
+    let bool5 = compareElementPartial (head a4) (generatedList) (bool4Adjusted)
+    let a5 = removeFirst a4
+    let bool5Adjusted = compareListBool (bool4Adjusted) (bool5)
+    print (resultCompleto)
+    print (bool2Adjusted)
+    print (bool3Adjusted)
+    print (bool4Adjusted)
+    print (bool5Adjusted)
+
+    let parcial = countResult bool5Adjusted
+    
+    print(show completo ++ " Completo, " ++ show (parcial-completo) ++ " Parcial")
+    -- let bool2Adjusted = compareListBool (listBool) (bool2)
+    -- show bool2Adjusted
+    -- return (True)
+    -- bool3 <- compareElementPartial (head a2) (b) (bool2Adjusted)
+    -- return bool3
     
